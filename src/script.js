4d27ec2c12bde1fd84ea591623fdcc6bb46d87a7
bonekyaw <phonenai2014@gmail.com>
2025-06-28 12:33:02 +0630

    Declaring variables & their scope

--- src/script.js
+++ src/script.js
@@ -1,7 +1,23 @@
+// https://github.com/Bonekyaw/In-Class-Programming-Basic
+
 const p = document.getElementById("para");
+const p2 = document.getElementById("num");
 const btn = document.getElementById("btn");
+{
+  var count = 1;
+}
 
 btn.onclick = () => {
+  // count = count / 2 ;
+  // count -= 1;
+  count++;
+  // const abc = (2 + 3) * 4 - 5 / 2;
   p.style.color = "red";
   p.style.fontSize = "15px";
+  p2.innerText = count;
 };
+
+// 2 + 3 * 4 - 5 / 2
+// 2 + ( 12 ) - ( 2.5 )
+// 14 - 2.5
+// 11.5
