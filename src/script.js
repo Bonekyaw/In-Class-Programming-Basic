640dc0f3ad6261028bfa8a94a4e2941b08b22d8d
bonekyaw <phonenai2014@gmail.com>
2025-06-29 11:38:00 +0630

    Copy primitive data

--- src/script.js
+++ src/script.js
@@ -3,18 +3,30 @@
 const p = document.getElementById("para");
 const p2 = document.getElementById("num");
 const btn = document.getElementById("btn");
-{
-  var count = 1;
-}
+// {
+//   var count = 1;
+// }
+
+// let name2 = "Phone";
+// let count = 1;
+
+let a = 2;
+let b = a;
 
 btn.onclick = () => {
   // count = count / 2 ;
   // count -= 1;
-  count++;
+  // count++;
   // const abc = (2 + 3) * 4 - 5 / 2;
   p.style.color = "red";
   p.style.fontSize = "15px";
-  p2.innerText = count;
+  // p2.innerText = name2 + "456";
+
+  console.log("a : ", a);
+  console.log("b : ", b);
+  a = 3;
+  console.log("a : ", a);
+  console.log("b : ", b);
 };
 
 // 2 + 3 * 4 - 5 / 2
