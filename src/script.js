df7d481aaa0425c18170b37e483d768cbfd88f09
bonekyaw <phonenai2014@gmail.com>
2025-07-05 11:33:36 +0630

    If condition

--- src/script.js
+++ src/script.js
@@ -3,6 +3,27 @@
 const p = document.getElementById("para");
 const p2 = document.getElementById("num");
 const btn = document.getElementById("btn");
+
+function changeText(title) {
+  // Parameter
+  p.innerText = title;
+}
+
+const add = (a, b) => {
+  return a + b;
+};
+
+btn.onclick = () => {
+  if (p.style.color === "gold") {
+    p.style.color = "red";
+  } else {
+    p.style.color = "gold";
+  }
+  changeText("Let's go!");
+  //const result = add(2, 3);
+  p2.innerText = add(2, 3);
+};
+
 // {
 //   var count = 1;
 // }
@@ -10,24 +31,24 @@ const btn = document.getElementById("btn");
 // let name2 = "Phone";
 // let count = 1;
 
-let a = 2;
-let b = a;
+// let a = 2;
+// let b = a;
 
-btn.onclick = () => {
-  // count = count / 2 ;
-  // count -= 1;
-  // count++;
-  // const abc = (2 + 3) * 4 - 5 / 2;
-  p.style.color = "red";
-  p.style.fontSize = "15px";
-  // p2.innerText = name2 + "456";
+// btn.onclick = () => {
+//   // count = count / 2 ;
+//   // count -= 1;
+//   // count++;
+//   // const abc = (2 + 3) * 4 - 5 / 2;
+//   p.style.color = "red";
+//   p.style.fontSize = "15px";
+//   // p2.innerText = name2 + "456";
 
-  console.log("a : ", a);
-  console.log("b : ", b);
-  a = 3;
-  console.log("a : ", a);
-  console.log("b : ", b);
-};
+//   console.log("a : ", a);
+//   console.log("b : ", b);
+//   a = 3;
+//   console.log("a : ", a);
+//   console.log("b : ", b);
+// };
 
 // 2 + 3 * 4 - 5 / 2
 // 2 + ( 12 ) - ( 2.5 )
